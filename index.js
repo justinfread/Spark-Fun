11e37b0bea7154d87c3be8ca769cf86cef2da489
JFread <justin.fread.m@gmail.com>
2023-01-28 16:31:25 -0600

    aded port variable

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ require('dotenv').config()
 //const fetch = require('node-fetch');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
-app.listen(3000, () => console.log('listening at 3000'));
-app.use(express.static('public'));
-app.use(express.json({limit: '1mb'}));
+app.listen(port, () => {
+  console.log('Starting server');
+});
+
+app.use(express.static('public')); // Host static files
+app.use(express.json({limit: '1mb'})); // Allow json to be passed from client to server
 
 // Set up a rout to receive data from client 
 app.post('/api', (request, response) => {
